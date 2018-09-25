51b097f15a60fe469e7c8c4b42043f44aea3d25e
Bruno Simões <brunossfer@gmail.com>
2018-09-25 00:03:00 -0300

    Add files via upload

--- _6grausdBraco.ino
+++ _6grausdBraco.ino
@@ -1,12 +1,12 @@
 /*
 vetor de ints 
 
-cintura             pino 3  faixa 10-100, pos ini 10
-ombro               pino 11 faixa 50-140, pos ini 100
-cotovelo            pino 9  faixa 50-170, pos ini 110
-pulso sobe desce    pino 7  faixa 10-150, pos ini 130
-pulso giratorio     pino 5  faixa 10-150, pos ini 150
-garra               pino 13 faixa 80-150, pos ini 100
+cintura             pino 3    faixa 10-100, pos ini 10
+ombro               pino 5    faixa 50-140, pos ini 100
+cotovelo            pino 7    faixa 50-170, pos ini 110
+pulso sobe desce    pino 9    faixa 10-150, pos ini 130
+pulso giratorio     pino 11   faixa 10-150, pos ini 150
+garra               pino 13    faixa 80-150, pos ini 100
 
 velocidade delay 15 rápido, 50 normal, 100 lento
 
@@ -26,11 +26,19 @@ Servo pulsoSD;
 Servo pulsoG;
 Servo garra;
 
+int destinoCintura = 10;
+int destinoOmbro = 100;
+int destinoCotovelo = 110;
+int destinoPulsoSD = 130;
+int destinoPulsoG = 150;
+int destinoGarra = 100;
+char velocidade = 'l';
+
 int passo(int origem, int destino){  
   return (destino - origem)/10;
   }
 
-void movimentar(int destinoCintura, int destinoOmbro, int destinoCotovelo, int destinoPulsoSD, int destinoPulsoG, int destinoGarra, char velocidade){
+void movimentar(){
   int vel, moverCintura, moverOmbro, moverCotovelo, moverPulsoSD, moverPulsoG, moverGarra;
 
   if(velocidade == 'l')
@@ -80,52 +88,49 @@ void movimentar(int destinoCintura, int destinoOmbro, int destinoCotovelo, int d
     garra.write(destinoGarra);
   }
 
-//---- tem que ser incremental, senão dá bosta por mexer rápido demais
-
 void setup() {
-  // associa servo aos nomes que os identificam
-  cintura.attach(3);
-  ombro.attach(11);
-  cotovelo.attach(9);
-  pulsoSD.attach(7);
-  pulsoG.attach(5);
-  garra.attach(13);
-
-// posicoes iniciais
-//  cintura.write(10);
-//  ombro.write(100);
-//  cotovelo.write(110);
-//  pulsoSD.write(130);
-//  pulsoG.write(150);
-//  garra.write(100);
-
   Serial.begin(9600);
-  Serial.flush();
+  // posicoes iniciais
+  cintura.write(10);
+  ombro.write(100);
+  cotovelo.write(110);
+  pulsoSD.write(90);
+  pulsoG.write(150);
+  garra.write(100);
+ // associa servo aos nomes que os identificam 
+  cintura.attach(3);
+  ombro.attach(5);
+  cotovelo.attach(7);
+  pulsoSD.attach(9);
+  pulsoG.attach(11);
+  garra.attach(13);
 }
+/* observar pulsoSD, parece ter algo estranho com ele */
+//--- ate aqui ta certo---//
 
 void loop() {  
   // suponho que serao enviados os 7 valores numericos em ordem, 
   // a posicao de cintura, ombro, cotovelo, pulso, pulso e garra
   // seguido da velocidade... suponho que vem o numero separado por espaço
   if(Serial.available() > 0){
-    Serial.println("This is an test");
-    int destinoCintura = Serial.parseInt();
+    Serial.println("E la vamos nos");
+    destinoCintura = Serial.parseInt();
         Serial.println(destinoCintura);
-    int destinoOmbro = Serial.parseInt();
+    destinoOmbro = Serial.parseInt();
         Serial.println(destinoOmbro);
-    int destinoCotovelo = Serial.parseInt();
+    destinoCotovelo = Serial.parseInt();
         Serial.println(destinoCotovelo);
-    int destinoPulsoSD = Serial.parseInt();
+    destinoPulsoSD = Serial.parseInt();
         Serial.println(destinoPulsoSD);
-    int destinoPulsoG = Serial.parseInt();
+    destinoPulsoG = Serial.parseInt();
         Serial.println(destinoPulsoG);
-    int destinoGarra = Serial.parseInt();
+    destinoGarra = Serial.parseInt();
         Serial.println(destinoGarra);
-    char velocidade = Serial.read();
+    velocidade = Serial.read();
         Serial.println(velocidade);
     Serial.flush();
 
-    // problema: serial não pega o char e tá lendo de novo e pagando só 0
+    // problema: serial não pega o char e tá lendo de novo e pegando so 0
     
     //movimentar(destinoCintura, destinoOmbro, destinoCotovelo, destinoPulsoSD, destinoPulsoG, destinoGarra, velocidade);
   }
---
+++ funciona.ino
@@ -0,0 +1,56 @@
+/*
+ Try this test sketch with the Servo library to see how your
+ servo responds to different settings, type a position
+ (0 to 180) or if you type a number greater than 200 it will be
+ interpreted as microseconds(544 to 2400), in the top of serial
+ monitor and hit [ENTER], start at 90 (or 1472) and work your
+ way toward zero (544) 5 degrees (or 50 micros) at a time, then
+ toward 180 (2400).
+*/
+#include <Servo.h>
+Servo cintura; 
+Servo ombro;
+Servo cotovelo;
+Servo pulsoSD;
+Servo pulsoG;
+Servo garra;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600); //set serial monitor baud rate to match
+  cintura.write(10);
+  ombro.write(100);
+  cotovelo.write(110);
+  pulsoSD.write(90);
+  pulsoG.write(150);
+  garra.write(100);
+  
+  cintura.attach(3);
+  ombro.attach(5);
+  cotovelo.attach(7);
+  pulsoSD.attach(9);
+  pulsoG.attach(11);
+  garra.attach(13);
+  prntIt();
+}
+
+void loop() {
+  // if there's any serial available, read it:
+  while (Serial.available() > 0) {
+
+    // look for the next valid integer in the incoming serial stream:
+    int pos = Serial.parseInt();
+    pos = constrain(pos, 0, 2400);
+    pulsoSD.write(pos);
+    prntIt();
+  }
+}
+void prntIt()
+{
+  Serial.print("  degrees = ");
+  Serial.print(pulsoSD.read());
+  Serial.print("\t");
+  Serial.print("microseconds =  ");
+  Serial.println(pulsoSD.readMicroseconds());
+} 
+
